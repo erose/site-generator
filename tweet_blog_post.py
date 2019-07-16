41aa9320c3b6a2f9286d7b213681a88b45383520
Eli Rose <really.eli@gmail.com>
2019-07-16 00:47:33 -0400

    Add tweet_blog_post.py

---
+++ tweet_blog_post.py
@@ -0,0 +1,21 @@
+import os, sys, os.path
+import twitter
+import util
+
+def get_title(filepath):
+    with open(filepath) as f:
+        return util.parse_md(f)['title']
+
+def get_url(filepath):
+    basename = os.path.basename(filepath)
+    return f"https://reallyeli.com/posts/{basename}"
+
+if __name__ == "__main__":
+    consumer_key = os.environ['TWITTER_API_CONSUMER_KEY']
+    consumer_secret = os.environ['TWITTER_API_CONSUMER_SECRET']
+    access_token_key = os.environ['TWITTER_API_ACCESS_TOKEN_KEY']
+    access_token_secret = os.environ['TWITTER_API_ACCESS_TOKEN_SECRET']
+
+    filepath = sys.argv[1]
+    api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret, access_token_key=access_token_key, access_token_secret=access_token_secret)
+    api.PostUpdate(f"New post: {get_title(filepath)} {get_url(filepath)}")
